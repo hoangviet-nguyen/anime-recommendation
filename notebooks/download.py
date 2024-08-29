4cbbaf3c999f86540960a2939c4ce62b902c426d
hoangviet.nguyen <hoangviet.nguyen@students.fhnw.ch>
2024-08-29 07:26:43 +0200

    changed dowload.py for linux

--- notebooks/download.py
+++ notebooks/download.py
@@ -13,15 +13,21 @@ if __name__ == "__main__":
 
     if not os.path.exists("./data"):
         os.mkdir(data_dir)
-        os.system(f"kaggle datasets download -d dbdmobile/myanimelist-dataset -p {data_dir}")
 
-        # Unzip the downloaded file
-        for file in os.listdir(data_dir):
-            if file.endswith(".zip"):
-                with zipfile.ZipFile(os.path.join(data_dir, file), 'r') as zip_ref:
-                    zip_ref.extractall(data_dir)
+        try:
+            os.system(f"kaggle datasets download -d dbdmobile/myanimelist-dataset -p {data_dir}")
 
-        # Optionally, you can remove the zip file after extraction
-        os.remove(os.path.join(data_dir, file))
+            # Unzip the downloaded file
+            for file in os.listdir(data_dir):
+                if file.endswith(".zip"):
+                    with zipfile.ZipFile(os.path.join(data_dir, file), 'r') as zip_ref:
+                        zip_ref.extractall(data_dir)
+
+            # Optionally, you can remove the zip file after extraction
+            os.remove(os.path.join(data_dir, file))
+            print("Completed the download")
+        except Exception as err:
+            print(err)
+            os.removedirs(data_dir)
 
 
